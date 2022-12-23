4bff94122e4219cfde262a769e54a3160b33e0b0
ginjake <hsginjake123@gmail.com>
2022-12-23 22:00:38 +0900

    validation

--- src/app/Http/Controllers/GiftController.php
+++ src/app/Http/Controllers/GiftController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use App\Models\Gift;
-use App\Http\Requests\GiftRequest;
+use App\Http\Requests\Gift\GiftUpdateRequest;
+use App\Http\Requests\Gift\GiftStoreRequest;
 class GiftController extends Controller
 {
     /**
@@ -31,7 +32,7 @@ class GiftController extends Controller
         return view('gift.edit', ['gift' => $gift]);
     }
 
-    public function update(GiftRequest $request)
+    public function update(GiftUpdateRequest $request)
     {
         $param = $request->input();
         $gift = Gift::find($param["id"]);
@@ -50,7 +51,7 @@ class GiftController extends Controller
         return redirect()->route('home');
     }
 
-    public function store(GiftRequest $request)
+    public function store(GiftStoreRequest $request)
     {
         $value = $request->input();
         $value['userId'] = Auth::user()->id;
---
+++ src/app/Http/Requests/Gift/GiftStoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Http\Requests\Gift;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Gift\BaseRequest;
+
+class GiftStoreRequest extends BaseRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        $rules = [
+            'url' => [
+                        'string',
+                        Rule::unique('gifts')->where(function ($query) {
+                            return $query->where('userId', Auth::user()->id);
+                        }),
+                        'regex:/(http).*(booth).*(items)/',
+                    ],
+        ];
+        return $rules;
+    }
+}
