bae2e0f5d7f027b7a59ba12da1a3b56b7a0c46f8
ginjake <hsginjake213@gmail.com>
2023-12-02 21:34:11 +0900

    広告機能追加

--- src/app/Models/Gift.php
+++ src/app/Models/Gift.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Gift extends Model
 {
+    use SoftDeletes;
     use HasFactory;
     protected $fillable = [
         'userId',
